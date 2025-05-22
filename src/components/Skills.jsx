28f613953d0595495166e026193ff8bd2de46dff
Igiraneza1 <igiranezaadeline@gmail.com>
2025-05-22 16:40:01 +0300

    commit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 
-import html from '../images/HTML.png';      
-import css from '../images/CSS.png';         
-import javascript from '../images/JAVASCRIPT.png'; 
-import react from '../images/REACT.png';
-import tailwind from '../images/TAILWIND.png';
+import html from '../images/HTML5.svg';      
+import css from '../images/CSS3.svg';         
+import javascript from '../images/JavaScript.svg'; 
+import react from '../images/React.svg';
+import tailwind from '../images/Tailwind-CSS.svg';
+import next from '../images/Next.js.svg'
+import git from '../images/Git.svg';
+import github from '../images/GitHub-Codespaces.svg';
+
+import typescript from '../images/TypeScript.svg'
 
 function Skills() {
   const techSkills = [
     { name: 'HTML', value: 95, img: html },
-    { name: 'CSS', value: 90, img: css },
-    { name: 'JavaScript', value: 85, img: javascript },
-    { name: 'React', value: 80, img: react },
     { name: 'Tailwind CSS', value: 90, img: tailwind },
+    { name: 'CSS', value: 80, img: css },
+    { name: 'JavaScript', value: 80, img: javascript },
+    { name: 'React', value: 80, img: react },
+    {name: 'Git', value: 70, img: git },
+    {name: 'GitHub', value: 70, img: github },
+    {name: 'Next.js', value: 60, img: next },
+    {name: 'TypeScript', value: 50, img: typescript },
+
   ];
 
   const softSkills = [
@@ -29,12 +39,12 @@ function Skills() {
     <section id="skills" className="min-h-screen bg-base-200 ">
       <h1 className="text-4xl md:text-5xl font-bold text-center text-primary mb-12">My Skills</h1>
 
-      <div className="grid grid-cols-2 lg:grid-cols-3 max-w-7xl mx-auto gap-10">
+      <div className="grid grid-cols-2 lg:grid-cols-4 max-w-7xl mx-auto gap-10">
         
         
-        <div className='grid col-span-2'>
+        <div className='grid col-span-3'>
           <h2 className="text-2xl font-bold mb-6 text-accent text-center">Technical Skills</h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-6">
             {techSkills.map((skill, index) => (
               <div
                 key={index}

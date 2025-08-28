871e7dca772547aa0a67716646d4890b0910a7e9
Igiraneza1 <igiranezaadeline@gmail.com>
2025-08-28 16:53:14 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -10,36 +10,23 @@ import Contact from './components/Contact';
 import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
 import TidioChat from './tidio/TidioChat';
+import ContactSection from './calender/page';
 
 function App() {
   return (
     <div className="bg-gray-200 text-cyan-950 dark:bg-cyan-950 dark:text-gray-200 min-h-screen">
       <Navbar />
       
-      <div id="/">
-        <Start />
-      </div>
-      <div id="home">
-        <Home />
-      </div>
-      <div id="about">
-        <About />
-      </div>
-      <div id="skills">
-        <Skills />
-      </div>
-      <div id="experience">
-        <Experience />
-      </div>
-      <div id="projects">
-        <Projects />
-      </div>
-      <div id="contact">
-        <Contact />
-      </div>
-
+      <div id="/"><Start /></div>
+      <div id="home"><Home /></div>
+      <div id="about"><About /></div>
+      <div id="skills"><Skills /></div>
+      <div id="experience"><Experience /></div>
+      <div id="projects"><Projects /></div>
+      <div id="contact"><Contact /></div>
       <Footer />
       <TidioChat />
+      <ContactSection />
       <ScrollToTop />
     </div>
   );
